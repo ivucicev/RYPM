56b6863f92b07ac2ee6d0238db88d874b77db70f
Ivan Vucicevic <ivucicev@Ivans-MacBook-Pro.local>
2025-07-22 11:15:45 +0200

    fix [ci]

--- docker/entrypoint-web.sh
+++ docker/entrypoint-web.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-mkdir -p /usr/share/nginx/html/assets/environment
+#mkdir -p /usr/share/nginx/html/assets/environment
 
-API_URL="${API_URL:-http://localhost:8080}"
+#API_URL="${API_URL:-http://localhost:8080}"
 
 
-ENV_FILE="/usr/share/nginx/html/assets/environment/env.js"
+#ENV_FILE="/usr/share/nginx/html/assets/environment/env.js"
 
-printf "window.env = {\n" > "$ENV_FILE"
-printf "  pocketbase: \"%s\",\n" "$API_URL" >> "$ENV_FILE"
-printf "  production: true\n" >> "$ENV_FILE"
-printf "};\n" >> "$ENV_FILE"
+#printf "window.env = {\n" > "$ENV_FILE"
+#printf "  pocketbase: \"%s\",\n" "$API_URL" >> "$ENV_FILE"
+#printf "  production: true\n" >> "$ENV_FILE"
+#printf "};\n" >> "$ENV_FILE"
 
-echo "Environment variables injected successfully!"
+#echo "Environment variables injected successfully!"
 
 exec nginx -g "daemon off;"

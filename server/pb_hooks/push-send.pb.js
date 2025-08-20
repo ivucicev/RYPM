37ea8c651cb1b61850c30bc3e0f5ad672acadb7d
ivucicev <ivucicev@gmail.com>
2025-08-20 13:41:48 +0200

    fix push testing [ci]

--- server/pb_hooks/push-send.pb.js
+++ server/pb_hooks/push-send.pb.js
@@ -4,21 +4,21 @@ routerAdd("POST", "/api/push-send", (e) => {
 
     let token = e.request.url.query().get("token")
     let duration = e.request.url.query().get("duration")
-    let body =  e.requestInfo().body;
+    let body = e.requestInfo().body;
 
     //send fire and forget
-    setTimeout(() => {
-        $http.send({
-            url: "https://faas-fra1-afec6ce7.doserverless.co/api/v1/web/fn-89192d39-2123-4764-971d-34f42a9a81ea/rypm/send",
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "authorization": `Bearer ${token}`
-            },
-            body: JSON.stringify(body)
-        });
-     
-    }, parseInt(duration))
+    sleep(parseInt(duration))
+
+    $http.send({
+        url: "https://faas-fra1-afec6ce7.doserverless.co/api/v1/web/fn-89192d39-2123-4764-971d-34f42a9a81ea/rypm/send",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(body)
+    });
+    
     return e.json(200, {})
 
 })

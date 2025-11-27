2e97c0b5c9227ae9fa53c74b2fe4267257d0e3f9
ivucicev <ivucicev@gmail.com>
2025-11-27 20:33:03 +0100

    feat auth screens design change [ci]

--- client/src/app/forgot-password/forgot-password.page.ts
+++ client/src/app/forgot-password/forgot-password.page.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { AccountService } from '../core/services/account.service';
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonItem, IonList, IonContent, IonBackButton, IonToolbar, IonTitle, IonButtons, IonButton, IonInput, IonFooter } from "@ionic/angular/standalone";
+import { IonHeader, IonItem, IonList, IonContent, IonBackButton, IonToolbar, IonTitle, IonButtons, IonButton, IonInput, IonFooter, IonIcon } from "@ionic/angular/standalone";
+import { Location } from '@angular/common';
 
 @Component({
     selector: 'app-forgot-password',
     templateUrl: 'forgot-password.page.html',
     styleUrls: ['./forgot-password.page.scss'],
     standalone: true,
-    imports: [IonButton, IonButtons, IonInput, IonFooter, IonToolbar, IonBackButton, IonContent, IonList, IonItem, IonHeader, TranslateModule, FormsModule],
+    imports: [IonIcon, IonButton, IonButtons, IonInput, IonFooter, IonToolbar, IonBackButton, IonContent, IonList, IonItem, IonHeader, TranslateModule, FormsModule],
 })
 export class ForgotPasswordPage {
 
@@ -17,7 +18,13 @@ export class ForgotPasswordPage {
     email: string;
 
     constructor(
-        private accountService: AccountService) { }
+        private accountService: AccountService,
+        private location: Location
+    ) { }
+
+    goBack() {
+        this.location.back();
+    }
 
     submit() {
         this.accountService.requestPasswordReset(this.email);
